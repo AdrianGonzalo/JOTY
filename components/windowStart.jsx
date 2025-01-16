c3d63c5452ed95b3ab78efdf3006e9403cb76667
Adrián <adriangonzalodev@gmail.com>
2025-01-16 15:51:13 +0100

    fix errors

--- components/windowStart.jsx
+++ components/windowStart.jsx
@@ -15,7 +15,7 @@ export default function WindowStart() {
     }
  
     return(
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-85 ">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-95 ">
             <div className="bg-[--background] w-[700px] h-[340px] rounded-xl p-8 flex flex-col z-40">
                 <p className="text-3xl flex justify-center mb-7">Pay attention Recruit</p>
                 <p className="text-xl mb-[50px] min-w-[572px]">This page is here to have a laugh and reward all those undercover secret agents who do a very important job for us. It is important to note that our intention is not to encourage or promote hatred towards any of these players, what we are looking for is to have a good time :)</p>
